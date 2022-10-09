cc293bb78bea7a26103a7e8fca366093cb8a5df8
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2022-10-09 11:09:25 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,8 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1',               env('SERVER', default='127.0.0.1') ]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ]
+ALLOWED_HOSTS        = ['*']#['localhost', 'localhost:85', '127.0.0.1',               env('SERVER', default='127.0.0.1') ]
+CSRF_TRUSTED_ORIGINS = ['http://*','https://*']#['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ]
 
 # Application definition
 
@@ -98,6 +98,8 @@ if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "postgresql":
         'PASSWORD': os.getenv('DB_PASS'     , 'pass'),
         'HOST'    : os.getenv('DB_HOST'     , 'localhost'),
         'PORT'    : os.getenv('DB_PORT'     , 3306),
+        'OPTIONS': {'sslmode': 'require'},
+          
         }, 
     }
 else:
