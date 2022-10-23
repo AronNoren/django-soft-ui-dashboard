0a015ba3f24bf8bde693d54ca20e092b59054059
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2022-10-23 15:04:38 +0200

    Update models.py

--- apps/models.py
+++ apps/models.py
@@ -51,5 +51,5 @@ class Lucy(models.Model):
 
     coin_name = models.CharField(max_length=100)
     date = models.DateField()
-    returns = models.DecimalField(max_digits=20, decimal_places=6, default=Decimal(0.00))
-    cumprod = models.DecimalField(max_digits=20, decimal_places=6, default=Decimal(0.00))
+    returns = models.DoubleField(null=True)
+    cumprod = models.DoubleField(null=True)
