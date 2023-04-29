e85e2091e94eb4c999bb95f210de7177dfa26d65
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-04-29 20:13:29 +0200

    Update chat.py

--- apps/api/chat.py
+++ apps/api/chat.py
@@ -6,24 +6,25 @@ import json
 api_key = os.getenv('GPT4Key')
 
 def chat(request):
-    if api_key is not None and request.method == 'POST':
+    if api_key is not None:
         openai.api_key = api_key
         user_input = request.POST.get('user_input')
 
-        response = openai.Completion.create(
-            engine= 'text-davinci-003',
-            prompt= user_input,
-            max_tokens=256,
-            temperature = 0.5
-        )
+        #response = openai.Completion.create(
+        #    engine= 'text-davinci-003',
+        #    prompt= user_input,
+        #    max_tokens=256,
+        #    temperature = 0.5
+        #)
 
         # convert the response to string, then parse as json
-        response_str = str(response)
-        response_json = json.loads(response_str)
+        #response_str = str(response)
+        #response_json = json.loads(response_str)
 
         # extract the text from the response
-        response_text = response_json["choices"][0]["text"]
+        #response_text = response_json["choices"][0]["text"]
 
-        return render(request, 'billing.html', {'response': response_text})
-    else:
-        return render(request, 'billing.html')
+        #return render(request, 'billing.html', {'response': response_text})
+        return " Hi Aron"
+    #else:
+        #return render(request, 'billing.html')
