0948d4dbb377086a3e08b2bf50f21ad8e2edaad8
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-04-29 17:54:12 +0200

    Update urls.py

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import re_path, path
 from django.views.decorators.csrf import csrf_exempt
 
-from apps.api.views import BookView
+from apps.api.views import BookView,ChatView
 from apps.api.chat import chat
 urlpatterns = [
 
 	re_path("books/((?P<pk>\d+)/)?", csrf_exempt(BookView.as_view())),
-        path('api/chat', csrf_exempt(chat), name='chat'),
+        path("chat/", ChatView.as_view(), name="chat"),
 
 ]
