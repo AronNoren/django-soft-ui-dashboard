102929e60ebfbf67d6ca86fe11d66ecde785d50d
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-04-29 20:06:19 +0200

    Update chat.py

--- apps/api/chat.py
+++ apps/api/chat.py
@@ -1,25 +1,29 @@
-import os
-import logging
-import requests
-from django.http import JsonResponse
+from django.http import HttpResponse
 import openai
-from django.shortcuts import render
-logger = logging.getLogger(__name__)
-api_key = os.getenv('GPT4Key')
-def chat(request):
-    chatresponse = None
+import os
+import json
 
+api_key = os.getenv('GPT4Key')
+
+def chat(request):
     if api_key is not None and request.method == 'POST':
-        message = "Hi GPT"#request.POST.get('message')
         openai.api_key = api_key
         user_input = request.POST.get('user_input')
+
         response = openai.Completion.create(
             engine= 'text-davinci-003',
             prompt= user_input,
             max_tokens=256,
             temperature = 0.5
-
         )
-        print(response)
-        return render((request,'billing.html',{response["choices"][0]["text"]}))
 
+        # convert the response to string, then parse as json
+        response_str = str(response)
+        response_json = json.loads(response_str)
+
+        # extract the text from the response
+        response_text = response_json["choices"][0]["text"]
+
+        return render(request, 'billing.html', {'response': response_text})
+    else:
+        return render(request, 'billing.html')
