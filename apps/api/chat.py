365cf99077a87a373b7061d86a55b1dd308c61b4
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-04-29 15:42:10 +0200

    Update chat.py

--- apps/api/chat.py
+++ apps/api/chat.py
@@ -1,21 +1,26 @@
-from django.http import JsonResponse
-import requests
-import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 def chat(request):
-    message = request.POST.get('message')
-    response = requests.post(
-        'https://api.openai.com/v1/engines/davinci-codex/completions',
-        headers={
-            'Authorization': 'Bearer ' + os.getenv('GPT4Key'),
-            'Content-Type': 'application/json'
-        },
-        json={
-            'prompt': message,
-            'max_tokens': 60,
-            'temperature': 0.5,
-        }
-    )
-    response.raise_for_status()
-    data = response.json()
-    return JsonResponse({'reply': data['choices'][0]['text'].strip()})
+    try:
+        message = request.POST.get('message')
+        response = requests.post(
+            'https://api.openai.com/v1/engines/davinci-codex/completions',
+            headers={
+                'Authorization': 'Bearer ' + os.getenv('GPT4Key'),
+                'Content-Type': 'application/json'
+            },
+            json={
+                'prompt': message,
+                'max_tokens': 60,
+                'temperature': 0.5,
+                'model': 'davinci-codex',
+            }
+        )
+        response.raise_for_status()
+        data = response.json()
+        return JsonResponse({'reply': data['choices'][0]['text'].strip()})
+    except Exception as e:
+        logger.error(f"Error during chat: {e}")
+        return JsonResponse({'error': 'An error occurred'}, status=500)
