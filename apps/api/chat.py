28ed25e04e7d7e0624dfa725a81b6c5824b3850f
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-04-29 20:51:41 +0200

    Update chat.py

--- apps/api/chat.py
+++ apps/api/chat.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse
 import openai
 import os
 import json
-
+from rest_framework.response import Response
 #api_key = os.getenv('GPT4Key')
 from django.http import HttpResponse
-
+from http import HTTPStatus
 def chat(request):
     user_input = request.POST.get('user_input')
-    return HttpResponse(user_input)
+    return Response(data={
+            'message': 'Record Created.',
+            'success': True
+        }, status=HTTPStatus.OK)
 
 #def chat(request):
 #    if api_key is not None:
