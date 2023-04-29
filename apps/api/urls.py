e25dd5cc027812a1fccea4e4d83f4a6f422ea8bd
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-04-29 20:35:06 +0200

    Update urls.py

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -6,6 +6,6 @@ from apps.api.chat import chat
 urlpatterns = [
 
 	re_path("books/((?P<pk>\d+)/)?", csrf_exempt(BookView.as_view())),
-        path("", chat, name="chat"),
+        path("chat/", chat, name="chat"),
 
 ]
