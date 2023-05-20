cf3d31810782684bf5e0bc71a82cdabcb4d76471
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-05-20 16:02:52 +0200

    Update urls.py

--- apps/dyn_datatables/urls.py
+++ apps/dyn_datatables/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import data_table_view, add_record, delete_record, edit_record, export, chat
+from .views import data_table_view, add_record, delete_record, edit_record, export, chat, chat_insurance
 
 urlpatterns = [
     path('datatb/<str:model_name>/', data_table_view),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('datatb/<str:model_name>/delete/<int:id>/', delete_record),
     path('datatb/<str:model_name>/export/', export),
     path('chat1/', chat),
+    path('chat_insurance/', chat_insurance),
 ]
