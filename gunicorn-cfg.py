bbc4659d9b9c622346c9ba05c67257c43b60caa3
Aron Noren <47273486+AronNoren@users.noreply.github.com>
2023-05-25 21:52:52 +0200

    Update gunicorn-cfg.py

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -4,8 +4,8 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 bind = '0.0.0.0:8080'
-workers = 2
-timeout = 120
+workers = 1
+timeout = 300
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
